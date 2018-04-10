85d72cff71977caad5bf1cef1715cd9c924ac93b
xyyxhcj <xyyxhcj@qq.com>
2018-04-10 11:21:02 +0800

    项目初始化

---
+++ src/main/java/com/github/hcj/TestApplication.java
@@ -0,0 +1,17 @@
+package com.github.hcj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口类
+ *
+ * @author xyyxhcj@qq.com
+ * @since 2018-04-10
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/github/hcj/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.github.hcj.controller;
+
+import com.github.hcj.utils.JsonUtils;
+import com.github.hcj.utils.VerifyCodeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author xyyxhcj@qq.com
+ * @since 2018-04-10
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping("index")
+    public String index() throws IOException {
+        File dir = new File("F:/verifies");
+        int w = 200, h = 80;
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(5);
+        File file = new File(dir, verifyCode + ".jpg");
+        VerifyCodeUtils.outputImage(w, h, file, verifyCode);
+        return JsonUtils.objectToJson(verifyCode);
+    }
+}
